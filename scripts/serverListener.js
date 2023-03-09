d7f170f3f4c416ec37e8927226efbe5242e2dc11
anthonydibi <ajdibiaggio@gmail.com>
2023-03-09 15:16:07 -0600

    Initial commit

---
+++ scripts/serverListener.js
@@ -0,0 +1,5 @@
+//content script that handles server events
+//responsibilities
+//1: set up web socket connection with the server
+//2: join chat rooms if possible
+//3: when joining a chat room, tell the background script to inject the chat window into the page
